97f5ec6ab549748992b35570c5f24f2f2cee98e4
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-02-28 23:08:06 -0600

    Some trials on white top part.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#000000" />
+
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon.png" />
         <link rel="theme-color" href="#fff" />
