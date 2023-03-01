ab61d63c590605e020dfa593f6103e0810ba5e4a
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-02-28 22:20:47 -0600

    Header and Navbar switched.

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Doraemon from "public/doraemon.png";
+import Image from "next/image";
+import { AiOutlineInstagram } from "react-icons/ai";
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-1">
+        <div className="w-8 h-8 overflow-hidden rounded-full">
+          <Image
+            src={Doraemon}
+            alt="Doraemon"
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <p className="font-mono text-xl">KwanBarber</p>
+      </div>
+      <a href="https://www.instagram.com/kwanbarber/" target="_blank">
+        <AiOutlineInstagram className="text-3xl text-white" />
+      </a>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import {
+  ClockIcon,
+  HomeModernIcon,
+  PhotoIcon,
+  ScissorsIcon,
+} from "@heroicons/react/24/outline";
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="grid grid-cols-4 gap-8">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="grid w-full p-4 border border-white rounded-xl h-14">
+          <HomeModernIcon />
+        </div>
+        <div className="font-mono text-xs text-white">Home</div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="grid w-full p-4 border border-white rounded-xl h-14">
+          <ScissorsIcon />
+        </div>
+
+        <div className="font-mono text-xs text-white">Bookings</div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="grid w-full p-4 border border-white rounded-xl h-14">
+          <ClockIcon />
+        </div>
+
+        <div className="font-mono text-xs text-white">History</div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="grid w-full p-4 border border-white rounded-xl h-14">
+          <PhotoIcon />
+        </div>
+
+        <div className="font-mono text-xs text-white">Gallery</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
