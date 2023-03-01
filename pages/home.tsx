f779d7271c6a5c89053e1f96fcde901521b58023
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-02-28 23:26:03 -0600

    Slight changes to CSS.

--- pages/home.tsx
+++ pages/home.tsx
@@ -13,17 +13,17 @@ const Home = (props: Props) => {
   const { data: session } = useSession();
   console.log(session);
   return (
-    <div className="relative flex flex-col justify-center py-5 px-7">
+    <div className="relative flex flex-col justify-center py-4 px-7">
       <Header />
       <div className="flex items-center">
         <div className="mt-4 font-mono ">{`Hey ${session?.user?.name}! 👋🏻`}</div>
       </div>
-      <div className="flex flex-col gap-6 my-5 mb-6">
+      <div className="z-20 flex flex-col gap-6 my-5 mb-6">
         <Appointment />
         <Card content="quote" />
         <Card content="count" />
       </div>
-      <div className="fixed bottom-8">
+      <div className="fixed z-50 bottom-8">
         <Navbar />
       </div>
     </div>
