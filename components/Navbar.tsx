5df10ee27e4b5325cbe61e0b8e7413c83f884e01
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-03-01 12:17:55 -0600

    Navbar with functionality.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,35 +4,74 @@ import {
   PhotoIcon,
   ScissorsIcon,
 } from "@heroicons/react/24/outline";
-import React from "react";
-
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { showState } from "@/atoms/showState";
+import { useRecoilState } from "recoil";
 type Props = {};
+type ShowType = "home" | "bookings" | "history" | "gallery";
 
 const Navbar = (props: Props) => {
+  const router = useRouter();
+  const [show, setShow] = useRecoilState(showState);
+  const handleItemClick = (route: String) => {
+    // Update the state to the clicked item index
+    // @ts-ignore
+    setShow(route);
+    router.push("/" + route);
+  };
   return (
     <div className="grid grid-cols-4 gap-8">
-      <div className="flex flex-col items-center justify-center gap-2">
-        <div className="grid w-full p-4 bg-black border border-white rounded-xl h-14">
+      <div
+        onClick={() => handleItemClick("home")}
+        className="flex flex-col items-center justify-center gap-2"
+      >
+        <div
+          className={`${
+            show === "home" ? "bg-white text-black" : "bg-black text-white"
+          } grid w-full p-4  border border-white rounded-xl h-14 transition duration-300 ease-in-out`}
+        >
           <HomeModernIcon />
         </div>
         <div className="font-mono text-xs text-white">Home</div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-2">
-        <div className="grid w-full p-4 bg-black border border-white rounded-xl h-14">
+      <div
+        onClick={() => handleItemClick("bookings")}
+        className="flex flex-col items-center justify-center gap-2"
+      >
+        <div
+          className={`${
+            show === "bookings" ? "bg-white text-black" : "bg-black text-white"
+          } grid w-full p-4  border border-white rounded-xl h-14 transition duration-300 ease-in-out`}
+        >
           <ScissorsIcon />
         </div>
 
         <div className="font-mono text-xs text-white">Bookings</div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-2">
-        <div className="grid w-full p-4 bg-black border border-white rounded-xl h-14">
+      <div
+        onClick={() => handleItemClick("history")}
+        className="flex flex-col items-center justify-center gap-2"
+      >
+        <div
+          className={`${
+            show === "history" ? "bg-white text-black" : "bg-black text-white"
+          } grid w-full p-4  border border-white rounded-xl h-14 transition duration-300 ease-in-out`}
+        >
           <ClockIcon />
         </div>
 
         <div className="font-mono text-xs text-white">History</div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-2">
-        <div className="grid w-full p-4 bg-black border border-white rounded-xl h-14">
+      <div
+        onClick={() => handleItemClick("gallery")}
+        className="flex flex-col items-center justify-center gap-2"
+      >
+        <div
+          className={`${
+            show === "gallery" ? "bg-white text-black" : "bg-black text-white"
+          } grid w-full p-4  border border-white rounded-xl h-14 transition duration-300 ease-in-out`}
+        >
           <PhotoIcon />
         </div>
 
--- pages/home.tsx
+++ pages/home.tsx
@@ -5,24 +5,27 @@ import Navbar from "@/components/Navbar";
 import Card from "@/components/Card";
 import Appointment from "@/components/Appointment";
 import Header from "@/components/Header";
+import { AnimatedSection } from "@/components/AnimatedSection";
 
 type Props = {};
 
 const Home = (props: Props) => {
   const router = useRouter();
   const { data: session } = useSession();
-  console.log(session);
   return (
     <div className="relative z-20 flex flex-col justify-center py-4 px-7">
       <Header />
-      <div className="flex items-center">
-        <div className="mt-4 font-mono ">{`Hey ${session?.user?.name}! 👋🏻`}</div>
-      </div>
-      <div className="flex flex-col gap-6 my-5 mb-6 ">
-        <Appointment />
-        <Card content="quote" />
-        <Card content="count" />
-      </div>
+      <AnimatedSection delay={0}>
+        <div className="flex items-center">
+          <div className="mt-4 font-mono ">{`Hey ${session?.user?.name}! 👋🏻`}</div>
+        </div>
+        <div className="flex flex-col gap-6 my-5 mb-6 ">
+          <Appointment />
+          <Card content="quote" />
+          <Card content="count" />
+        </div>
+      </AnimatedSection>
+
       <div className="fixed z-50 bottom-8 ">
         <Navbar />
       </div>
