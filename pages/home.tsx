b1a0dd5f3a2cbd08faef65e0abb635f8c25e60c8
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-02-28 22:21:12 -0600

    Home page implementation.

---
+++ pages/home.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Navbar from "@/components/Navbar";
+import Card from "@/components/Card";
+import Appointment from "@/components/Appointment";
+import Header from "@/components/Header";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <div className="flex flex-col justify-center py-10 px-7">
+      <Header />
+      <div className="flex items-center">
+        <div className="mt-4 font-mono ">{`Hey ${session?.user?.name}! 👋🏻`}</div>
+      </div>
+      <div className="flex flex-col gap-6 my-5 mb-6">
+        <Appointment />
+        <Card content="quote" />
+        <Card content="count" />
+      </div>
+      <Navbar />
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context: any) => {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
+
+export default Home;
