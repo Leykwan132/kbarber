8334d4aa916b015e16ab3dae7c143f4ce20d1fe2
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2023-03-02 00:43:43 -0600

    Mini card first version implemented.

---
+++ components/MiniCard.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+
+export interface MiniCardProps {
+  date: string;
+  time: string;
+  client: string;
+  after_side?: string;
+  after_front?: string;
+}
+
+type InputType = "gallery" | "history";
+type Props = {
+  type: InputType;
+  data: MiniCardProps;
+};
+
+const MiniCard = ({ type, data }: Props) => {
+  if (type === "history") {
+    return (
+      <div className="flex justify-between p-4 font-mono text-white bg-gray-800 rounded-xl">
+        <div className="flex flex-col items-start justify-start gap-3 text-sm">
+          <div>{data?.date}</div>
+          <div>Time Taken: {data?.time}</div>
+        </div>
+        <div className="relative overflow-hidden bg-white border-gray-300 rounded-full w-14 h-14 ">
+          <Image
+            className="object-contain"
+            src={data?.after_front}
+            fill
+            alt="picture"
+          />
+        </div>
+      </div>
+    );
+  }
+  return <div>MiniCard</div>;
+};
+
+export default MiniCard;
--- pages/history.tsx
+++ pages/history.tsx
@@ -1,20 +1,99 @@
-import Appointment from "@/components/Appointment";
-import Card from "@/components/Card";
-import Header from "@/components/Header";
-import Navbar from "@/components/Navbar";
+import MiniCard, { MiniCardProps } from "@/components/MiniCard";
 import React from "react";
 
 type Props = {};
-
+const sampleData: MiniCardProps[] = [
+  {
+    client: "Aidiel",
+    date: "Feb 23",
+    time: "30 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://images.squarespace-cdn.com/content/v1/5bd1d89365a707203856dda2/1572124949771-2WU7A0AUN7W0F1I9UKC1/IMG_2007.jpeg?format=2500w",
+  },
+  {
+    client: "Daniel",
+    date: "Feb 30",
+    time: "25 min 28 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+  {
+    client: "Dinesh",
+    date: "Feb 22",
+    time: "26 min 20 sec",
+    after_side:
+      "https://m.media-amazon.com/images/M/MV5BZGM5MWM1MmUtZDM0ZC00Y2EwLWEwMzQtM2QwZDU4Yjc5ODJmXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_.jpg",
+    after_front:
+      "https://www.beyoung.in/blog/wp-content/uploads/2020/04/general-min-1-853x1024.jpg",
+  },
+];
 const history = (props: Props) => {
   return (
     <div className="relative flex flex-col justify-center bg-black px-7 ">
-      <div className="z-20">
-        <div className="mt-4 font-mono text-white ">History</div>{" "}
-        <div className="flex flex-col gap-6 my-5 mb-6 ">
-          <Appointment />
-          <Card content="quote" />
-          <Card content="count" />
+      <div className="z-20 overflow-y-scroll max-h-[660px] scrollbar-hide">
+        <div className="mt-4 font-mono text-white">Gallery</div>{" "}
+        <div className="flex flex-col gap-4 mt-2">
+          {sampleData.map((data, index) => (
+            <MiniCard key={index} data={data} type="history" />
+          ))}
         </div>
       </div>
     </div>
